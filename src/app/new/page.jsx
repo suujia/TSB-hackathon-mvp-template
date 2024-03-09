3976a38dab5c2dc9298c2aae99486d92a7313d7a
Susie <suujia@icloud.com>
2024-03-09 18:47:56 +0100

    Very simple full implementation of mini twitter

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,12 +1,31 @@
+import { redirect } from "next/navigation";
 import Link from "next/link";
+import prisma from "../../db";
 
-export default function Page() {
+async function createTweet(data) {
+  "use server";
+  const content = data.get("content")?.valueOf();
+  if (typeof content !== "string" || content.length === 0) {
+    return;
+  }
+
+  await prisma.tweet.create({
+    data: {
+      content,
+    },
+  });
+  redirect("/");
+}
+
+export default function NewTweetPage() {
   return (
     <div className="container">
-      <h1>New Todos</h1>
-      <Link href="/" className="button">
-        Go back
-      </Link>
+      <h1>New Tweet</h1>
+      <Link href="/">Back</Link>
+      <form action={createTweet}>
+        <input type="text" name="content" />
+        <button type="submit">Tweet</button>
+      </form>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,20 @@
-import Image from "next/image";
 import Link from "next/link";
+import prisma from "../db";
 
-export default function Page() {
+export default async function Page() {
+  const tweets = await prisma.tweet.findMany();
+
   return (
     <div className="container">
-      <h1>Hello World</h1>
-      <Link href="/new" className="button">
-        Create New Todo
-      </Link>
+      <h1>Mini Twitter</h1>
+      <Link href="/new">New Tweet</Link>
+      <ul>
+        {tweets.map((tweet) => (
+          <div className="tweet" key={tweet.id}>
+            {tweet.content}
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
